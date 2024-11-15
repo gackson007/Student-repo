ee37fda401c3bb45debf9272d99f0f1f6e71feaf
gackson007 <gabrielomokaro01@gmail.com>
2024-11-16 00:33:30 +0100

    Added some packages to the extra folders

---
+++ src/com/studentmanagementsystem/dto/StudentDto.java
@@ -0,0 +1,49 @@
+//package com.studentmanagementsystem.dto;
+//
+//public class StudentDto {
+//    private String name;
+//    private String email;
+//    private String course;
+//
+//    public StudentDto() {
+//    }
+//
+//    public StudentDto(String name, String email, String course) {
+//        this.name = name;
+//        this.email = email;
+//        this.course = course;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getCourse() {
+//        return course;
+//    }
+//
+//    public void setCourse(String course) {
+//        this.course = course;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "StudentDto{" +
+//                "name='" + name + '\'' +
+//                ", email='" + email + '\'' +
+//                ", course='" + course + '\'' +
+//                '}';
+//    }
+//}
